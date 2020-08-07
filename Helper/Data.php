9a7eea65c25eca983b4f73d4b7c6f4e6c79d9fe2
Ricardo Martins <ricardo@ricardomartins.net.br>
2020-08-07 11:52:09 +1000

    Pequena correção na validação de CPF
    Corrige problema de compilação
    Deprecated Functionality: Array and string offset access syntax with curly braces is deprecated
    
    Fixes #54

--- Helper/Data.php
+++ Helper/Data.php
@@ -45,10 +45,10 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
         // Faz o calculo para validar o CPF
         for ($t = 9; $t < 11; $t++) {
             for ($d = 0, $c = 0; $c < $t; $c++) {
-                $d += $cpf{$c} * (($t + 1) - $c);
+                $d += $cpf[$c] * (($t + 1) - $c);
             }
             $d = ((10 * $d) % 11) % 10;
-            if ($cpf{$c} != $d) {
+            if ($cpf[$c] != $d) {
                 return false;
             }
         }
